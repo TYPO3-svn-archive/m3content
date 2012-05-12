678ddf6f5b936be1cb5a047967bd651272d84fec
gabe <gabe@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-12 12:19:49 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/m3content/tags/Meridian3_v0.9.0@61943 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "m3content".
 #
-# Auto generated 17-02-2012 22:20
+# Auto generated 12-05-2012 14:15
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"5367";s:10:"README.txt";s:4:"9fa9";s:22:"class.tx_m3content.php";s:4:"70ec";s:21:"ext_conf_template.txt";s:4:"66fe";s:12:"ext_icon.gif";s:4:"1557";s:17:"ext_localconf.php";s:4:"5663";s:14:"ext_tables.php";s:4:"94b8";s:14:"ext_tables.sql";s:4:"461a";s:13:"locallang.xml";s:4:"8a5a";s:16:"locallang_db.xml";s:4:"26f9";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_m3content_pi1.php";s:4:"5fff";s:38:"pi1/class.tx_m3content_pi1_wizicon.php";s:4:"ddae";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"47e8";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"83aa";s:10:"README.txt";s:4:"9fa9";s:22:"class.tx_m3content.php";s:4:"70ec";s:21:"ext_conf_template.txt";s:4:"66fe";s:12:"ext_icon.gif";s:4:"1557";s:17:"ext_localconf.php";s:4:"5663";s:14:"ext_tables.php";s:4:"94b8";s:14:"ext_tables.sql";s:4:"461a";s:13:"locallang.xml";s:4:"8a5a";s:16:"locallang_db.xml";s:4:"26f9";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_m3content_pi1.php";s:4:"5fff";s:38:"pi1/class.tx_m3content_pi1_wizicon.php";s:4:"ddae";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"47e8";}',
 	'suggests' => array(
 	),
 );
